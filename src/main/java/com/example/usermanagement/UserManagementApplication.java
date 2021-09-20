f77ddde18b81569a3c38ef01e59f3b3bf09b6037
sitetester <sitetester2010@gmail.com>
2021-09-20 19:17:51 +0300

    logging support added

--- src/main/java/com/example/usermanagement/UserManagementApplication.java
+++ src/main/java/com/example/usermanagement/UserManagementApplication.java
@@ -1,13 +1,16 @@
 package com.example.usermanagement;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
+		log.info("Application started successfully!");
 	}
 
 }
